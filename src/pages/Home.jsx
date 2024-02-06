411c1808d768cd89b2123cfac5e501c58ffc7306
kewii33 <kewiii071@gmail.com>
2024-02-06 15:37:17 +0900

    ex2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo } from '../redux/modules/todos';
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = () => {
-  return <div></div>;
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const todos = useSelector((state) => state.todos.todos);
+  const dispatch = useDispatch();
+
+  const onSubmitHnadler = (e) => {
+    e.preventDefault();
+    dispatch(
+      addTodo({
+        id: uuidv4(),
+        title,
+        body,
+        isDone: false,
+      })
+    );
+  };
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const bodyChangeHandler = (e) => {
+    setBody(e.target.value);
+  };
+
+  return (
+    <div>
+      <header>TodoList</header>
+      <main>
+        <form className="inputArea" onSubmit={onSubmitHnadler}>
+          <div>
+            제목 :{' '}
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => titleChangeHandler(e)}
+            ></input>
+          </div>
+          <div>
+            내용 :{' '}
+            <input
+              type="text"
+              value={body}
+              onChange={(e) => bodyChangeHandler(e)}
+            ></input>
+          </div>
+          <button>추가하기</button>
+        </form>
+        <div className="todoList">
+          <div className="ActiveList">
+            {todos.filter(
+              function (todo) {
+                return todo.isDone === false;
+              }.map(function (todo) {
+                return (
+                  <div key={todo.id}>
+                    <div>{todo.title}</div>
+                    <div>{todo.body}</div>
+                    <button>삭제하기</button>
+                    <button>완료</button>
+                  </div>
+                );
+              })
+            )}
+          </div>
+          <div className="doneList">
+            {todos.filter(
+              function (todo) {
+                return todo.isDone === true;
+              }.map(function (todo) {
+                return (
+                  <div key={todo.id}>
+                    <div>{todo.title}</div>
+                    <div>{todo.body}</div>
+                    <button>삭제하기</button>
+                    <button>취소</button>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </div>
+      </main>
+      <footer></footer>
+    </div>
+  );
 };
 
 export default Home;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,26 +1,62 @@
 // import uuid from "react-uuid";
-import shortid from "shortid";
+import { v4 as uuidv4 } from 'uuid';
 
-const initialState = [
-  {
-    id: shortid.generate(),
-    title: "",
-    body: "",
-    isDone: false,
-  },
-];
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+const SWITCH_TODO = 'SWITCH_TODO';
+
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload: payload,
+  };
+};
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload: payload,
+  };
+};
+export const switchTodo = (payload) => {
+  return {
+    type: SWITCH_TODO,
+    payload: payload,
+  };
+};
+
+const initialState = {
+  todos: [
+    {
+      id: uuidv4(),
+      title: '',
+      body: '',
+      isDone: false,
+    },
+  ],
+};
 
 // 리듀서
 const todos = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_TODO":
-      return; //TODO: 여기 작성
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      }; //TODO: 여기 작성
 
-    case "DELETE_TODO":
-      return; //TODO: 여기 작성
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      }; //TODO: 여기 작성
 
-    case "SWITCH_TODO":
-      return; //TODO: 여기 작성
+    case 'SWITCH_TODO':
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, isDone: !todo.isDone } : todo
+        ),
+      }; //TODO: 여기 작성
 
     default:
       return state;
